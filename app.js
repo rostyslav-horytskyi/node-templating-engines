1ea84af537199b55e0b2b952e02cc56604ee58bd
rostyslav-horytskyi <gorzer.rostyslav@gmail.com>
2021-10-22 13:54:03 +0300

    small node project without connecting to database

--- app.js
+++ app.js
@@ -2,22 +2,24 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+
+const errorController = require('./controllers/error');
+const db = require('./util/database');
+
 const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', 'views');
 
-const adminData = require('./routes/admin');
+const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 
-app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/admin', adminData.routes);
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use((req, res, next) => {
-    res.status(404).render('404', {pageTitle: '404'});
-});
+app.use(errorController.get404);
 
 app.listen(4000);
